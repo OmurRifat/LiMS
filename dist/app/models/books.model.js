ae1522677eb9590eba269f53d50adc95443f4fd8
H. M. Jabed Omur Rifat <codemen.jabed@gmail.com>
2025-07-19 23:44:56 +0600

    post hock added

--- dist/app/models/books.model.js
+++ dist/app/models/books.model.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Book = void 0;
 const mongoose_1 = require("mongoose");
@@ -14,5 +23,15 @@ const bookSchema = new mongoose_1.Schema({
 }, {
     timestamps: true,
     versionKey: false
+});
+bookSchema.post("findOneAndUpdate", function (doc) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (doc && doc.copies === 0 && doc.available !== false) {
+            yield doc.updateOne({ available: false });
+        }
+        else if (doc && doc.copies > 0 && doc.available !== true) {
+            yield doc.updateOne({ available: true });
+        }
+    });
 });
 exports.Book = (0, mongoose_1.model)('Book', bookSchema);
--- src/app/models/books.model.ts
+++ src/app/models/books.model.ts
@@ -3,8 +3,8 @@ import { IBooks } from "../interfaces/books.interface";
 // Defining the schema for the Book model
 const bookSchema = new Schema<IBooks>({
     title: { type: String, required: true },
-    author: {type: String, required: true},
-    genre: {type: String, enum: ["FICTION", "NON_FICTION", "SCIENCE", "HISTORY", "FANTASY", "BIOGRAPHY"], required: true},
+    author: { type: String, required: true },
+    genre: { type: String, enum: ["FICTION", "NON_FICTION", "SCIENCE", "HISTORY", "FANTASY", "BIOGRAPHY"], required: true },
     isbn: { type: String, required: true, unique: true },
     description: { type: String },
     copies: { type: Number, required: true, min: 0 },
@@ -12,6 +12,14 @@ const bookSchema = new Schema<IBooks>({
 }, {
     timestamps: true,
     versionKey: false
+});
+
+bookSchema.post("findOneAndUpdate", async function (doc) {
+    if (doc && doc.copies === 0 && doc.available !== false) {
+        await doc.updateOne({ available: false });
+    } else if (doc && doc.copies > 0 && doc.available !== true) {
+        await doc.updateOne({ available: true });
+    }
 });
 
 export const Book = model('Book', bookSchema);
